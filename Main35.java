e2fc894fe69fcb45598712f7f21ca66309a1d6a7
Rafal <rafalstalka85@gmail.com>
2019-09-01 14:39:24 +0200

    third commit

---
+++ Main35.java
@@ -0,0 +1,16 @@
+
+public class Main5 {
+
+	public static void main(String[] args) {
+		int a = 1;
+		int b;
+		System.out.println(++a);
+		System.out.println(a++);
+		System.out.println(a);
+		b=a++;
+		System.out.println(b);
+		b=++a;
+		System.out.println(++a);
+	}
+
+}
